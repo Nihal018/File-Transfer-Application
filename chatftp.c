aa1f9602340aa69b0fe0e946b786bdc8da6d8a27
Nihal018 <nihalchoutapelly@gmail.com>
2024-04-06 00:07:39 +0530

    updated chatftp.c

--- chatftp.c
+++ chatftp.c
@@ -13,16 +13,18 @@
 #define TFTP_PACKET 516
 #define MODE "octet"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     struct addrinfo hints, *res;
     int clientSocket, recvbytes;
     unsigned char response[TFTP_PACKET];
-    short blockno = 0; // Initialized to 0 for the first ACK for RRQ
+    short blockno = 0; // Initialized to 0 for the first ACK
     int elementsWritten;
     unsigned char lastAckPacket[4];
     size_t lastAckPacketLen = 0;
 
-    if (argc != 4) {
+    if (argc != 4)
+    {
         fprintf(stderr, "Usage: %s <ServerIP> <ServerPort> <Filename>\n", argv[0]);
         exit(1);
     }
@@ -32,7 +34,8 @@ int main(int argc, char *argv[]) {
     char *filename = argv[3];
 
     FILE *fd = fopen(filename, "wb");
-    if (!fd) {
+    if (!fd)
+    {
         perror("Cannot open file");
         exit(EXIT_FAILURE);
     }
@@ -41,23 +44,26 @@ int main(int argc, char *argv[]) {
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_DGRAM;
 
-    if (getaddrinfo(serverIP, port, &hints, &res) != 0) {
+    if (getaddrinfo(serverIP, port, &hints, &res) != 0)
+    {
         perror("getaddrinfo failed");
         exit(EXIT_FAILURE);
     }
 
     clientSocket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (clientSocket == -1) {
+    if (clientSocket == -1)
+    {
         perror("Error creating client socket");
         exit(EXIT_FAILURE);
     }
 
     // Set the socket timeout for recvfrom
     struct timeval timeout;
-    timeout.tv_sec = 0; // 5 seconds timeout
-    timeout.tv_usec = 500000;
+    timeout.tv_sec = 5; // 5 seconds timeout
+    timeout.tv_usec = 0;
 
-    if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
+    if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+    {
         perror("setsockopt failed");
         exit(EXIT_FAILURE);
     }
@@ -71,24 +77,32 @@ int main(int argc, char *argv[]) {
     strcpy((char *)response + 3 + strlen(filename), MODE);
 
     // Send the RRQ packet
-    if (sendto(clientSocket, response, packet_len, 0, res->ai_addr, res->ai_addrlen) < 0) {
+    if (sendto(clientSocket, response, packet_len, 0, res->ai_addr, res->ai_addrlen) < 0)
+    {
         perror("sendto failed");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
+    while (1)
+    {
         recvbytes = recvfrom(clientSocket, response, TFTP_PACKET, 0, NULL, NULL);
-        if (recvbytes < 0) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        if (recvbytes < 0)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
                 printf("Timeout reached. Resending last ACK.\n");
-                if (lastAckPacketLen > 0) {
-                    if (sendto(clientSocket, lastAckPacket, lastAckPacketLen, 0, res->ai_addr, res->ai_addrlen) < 0) {
+                if (lastAckPacketLen > 0)
+                {
+                    if (sendto(clientSocket, lastAckPacket, lastAckPacketLen, 0, res->ai_addr, res->ai_addrlen) < 0)
+                    {
                         perror("sendto failed");
                         exit(EXIT_FAILURE);
                     }
                 }
                 continue;
-            } else {
+            }
+            else
+            {
                 perror("recvfrom failed");
                 exit(EXIT_FAILURE);
             }
@@ -96,14 +110,17 @@ int main(int argc, char *argv[]) {
 
         memcpy(&opcode, response, sizeof(short));
         opcode = ntohs(opcode);
-        if (opcode == 3) { // DATA packet
+        if (opcode == 3)
+        { // DATA packet
             short receivedBlockNo;
             memcpy(&receivedBlockNo, response + 2, sizeof(short));
             receivedBlockNo = ntohs(receivedBlockNo);
 
-            if (receivedBlockNo == blockno + 1) {
+            if (receivedBlockNo == blockno + 1)
+            {
                 elementsWritten = fwrite(response + 4, 1, recvbytes - 4, fd);
-                if (elementsWritten < recvbytes - 4) {
+                if (elementsWritten < recvbytes - 4)
+                {
                     fprintf(stderr, "Error writing to file\n");
                     break;
                 }
@@ -112,18 +129,22 @@ int main(int argc, char *argv[]) {
 
                 // Prepare ACK packet
                 memset(lastAckPacket, 0, sizeof(lastAckPacket));
-                opcode = htons(4); // ACK opcode
+                opcode = htons(4);                 // ACK opcode
+                short netBlockNo = htons(blockno); // Convert block number to network byte order
                 memcpy(lastAckPacket, &opcode, sizeof(short));
-                memcpy(lastAckPacket + 2, &blockno, sizeof(short)); // Correctly store block number in network byte order
+                memcpy(lastAckPacket + 2, &netBlockNo, sizeof(netBlockNo)); // Include block number
                 lastAckPacketLen = 4;
 
                 // Send ACK packet
-                if (sendto(clientSocket, lastAckPacket, lastAckPacketLen, 0, res->ai_addr, res->ai_addrlen) < 0) {
+                if (sendto(clientSocket, lastAckPacket, lastAckPacketLen, 0, res->ai_addr, res->ai_addrlen) < 0)
+                {
                     perror("sendto failed");
                     exit(EXIT_FAILURE);
                 }
 
-                if (recvbytes < 516) { // Last packet
+                if (recvbytes < 516)
+                { // Last packet
+                    printf("File transfer complete.\n");
                     break;
                 }
             }
--- tftpclient.c
+++ tftpclient.c
@@ -6,8 +6,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <time.h>
 #include <netdb.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
 #define TFTP_PACKET 516
@@ -18,16 +18,14 @@ int main(int argc, char *argv[])
     struct addrinfo hints, *res;
     int clientSocket, recvbytes;
     unsigned char response[TFTP_PACKET];
-    struct timespec start, end;
-    unsigned char payload[TFTP_PACKET];
-    char *mode;
-    int num_packets;
-    short blockno = 1;
+    short blockno = 0; // Initialized to 0 for the first ACK
     int elementsWritten;
+    unsigned char lastAckPacket[4];
+    size_t lastAckPacketLen = 0;
 
     if (argc != 4)
     {
-        fprintf(stderr, "Usage: %s <ServerIP> <ServerPort> <P> <TTL> <NumPackets>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <ServerIP> <ServerPort> <Filename>\n", argv[0]);
         exit(1);
     }
 
@@ -36,84 +34,126 @@ int main(int argc, char *argv[])
     char *filename = argv[3];
 
     FILE *fd = fopen(filename, "wb");
+    if (!fd)
+    {
+        perror("Cannot open file");
+        exit(EXIT_FAILURE);
+    }
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_DGRAM;
 
-    getaddrinfo(serverIP, port, &hints, &res);
+    if (getaddrinfo(serverIP, port, &hints, &res) != 0)
+    {
+        perror("getaddrinfo failed");
+        exit(EXIT_FAILURE);
+    }
 
     clientSocket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (clientSocket == -1)
     {
         perror("Error creating client socket");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    printf("Client: socket has been created\n");
+    // Set the socket timeout for recvfrom
+    struct timeval timeout;
+    timeout.tv_sec = 0; // 5 seconds timeout
+    timeout.tv_usec = 500000;
 
-    short opcode = 1; // first write implement for the reading files . RRQ
+    if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+    {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // Prepare the RRQ packet
+    short opcode = htons(1); // RRQ opcode in network byte order
+    size_t packet_len = 2 + strlen(filename) + 1 + strlen(MODE) + 1;
     memset(response, 0, TFTP_PACKET);
     memcpy(response, &opcode, sizeof(short));
+    strcpy((char *)response + 2, filename);
+    strcpy((char *)response + 3 + strlen(filename), MODE);
 
-    memcpy(response + 2, filename, strlen(filename));
-    // Add a null byte after the filename
-    response[2 + strlen(filename)] = '\0';
-    // Copy the mode
-    memcpy(response + 3 + strlen(filename), MODE, strlen(MODE));
-    // Add a final null byte after the mode
-    response[3 + strlen(filename) + strlen(MODE)] = '\0';
-
-    size_t packet_len = 2 + strlen(filename) + 1 + strlen(MODE) + 1;
-    sendto(clientSocket, response, packet_len, 0, res->ai_addr, res->ai_addrlen);
-
-    blockno = 0;
+    // Send the RRQ packet
+    if (sendto(clientSocket, response, packet_len, 0, res->ai_addr, res->ai_addrlen) < 0)
+    {
+        perror("sendto failed");
+        exit(EXIT_FAILURE);
+    }
 
     while (1)
     {
         recvbytes = recvfrom(clientSocket, response, TFTP_PACKET, 0, NULL, NULL);
-        if (recvbytes == 516)
+        if (recvbytes < 0)
         {
-            memcpy(&opcode, response, sizeof(short));
-            if (opcode == 3)
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
             {
-                elementsWritten = fwrite(response + 4, sizeof(unsigned char), recvbytes - 4, fd);
-                if (elementsWritten != recvbytes - 4)
+                printf("Timeout reached. Resending last ACK.\n");
+                if (lastAckPacketLen > 0)
                 {
-                    // Handle error
-                    printf("Error writing to file.\n");
+                    if (sendto(clientSocket, lastAckPacket, lastAckPacketLen, 0, res->ai_addr, res->ai_addrlen) < 0)
+                    {
+                        perror("sendto failed");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                continue;
+            }
+            else
+            {
+                perror("recvfrom failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        memcpy(&opcode, response, sizeof(short));
+        opcode = ntohs(opcode);
+        if (opcode == 3)
+        { // DATA packet
+            short receivedBlockNo;
+            memcpy(&receivedBlockNo, response + 2, sizeof(short));
+            receivedBlockNo = ntohs(receivedBlockNo);
+
+            if (receivedBlockNo == blockno + 1)
+            {
+                elementsWritten = fwrite(response + 4, 1, recvbytes - 4, fd);
+                if (elementsWritten < recvbytes - 4)
+                {
+                    fprintf(stderr, "Error writing to file\n");
+                    break;
                 }
 
-                memset(response, 0, BUFFER_SIZE);
-                opcode = 4;
-                memcpy(response, &opcode, sizeof(short));
-                blockno++;
-                memcpy(response + 2, &blockno, sizeof(blockno));
-                sendto(clientSocket, response, 4, 0, res->ai_addr, res->ai_addrlen);
-            }
-        }
-        else
-        {
-            // end of transmission
-            elementsWritten = fwrite(response + 4, sizeof(unsigned char), recvbytes - 4, fd);
-            if (elementsWritten != recvbytes - 4)
-            {
-                // Handle error
-                printf("Error writing to file.\n");
-            }
+                blockno = receivedBlockNo; // Update block number for ACK
 
-            memset(response, 0, BUFFER_SIZE);
-            opcode = 4;
-            memcpy(response, &opcode, sizeof(short));
-            blockno++;
-            memcpy(response + 2, &blockno, sizeof(blockno));
-            sendto(clientSocket, response, 4, 0, res->ai_addr, res->ai_addrlen);
-            break;
+                // Prepare ACK packet
+                memset(lastAckPacket, 0, sizeof(lastAckPacket));
+                opcode = htons(4);                 // ACK opcode
+                short netBlockNo = htons(blockno); // Convert block number to network byte order
+                memcpy(lastAckPacket, &opcode, sizeof(short));
+                memcpy(lastAckPacket + 2, &netBlockNo, sizeof(netBlockNo)); // Include block number
+                lastAckPacketLen = 4;
+
+                // Send ACK packet
+                if (sendto(clientSocket, lastAckPacket, lastAckPacketLen, 0, res->ai_addr, res->ai_addrlen) < 0)
+                {
+                    perror("sendto failed");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (recvbytes < 516)
+                { // Last packet
+                    printf("File transfer complete.\n");
+                    break;
+                }
+            }
         }
     }
-    fclose(fd);
 
+    fclose(fd);
     close(clientSocket);
+    freeaddrinfo(res);
+
     return 0;
 }
